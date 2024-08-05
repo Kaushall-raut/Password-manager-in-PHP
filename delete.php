9b8ffaa0837fa53e1f93ebb8615f2dcee2511cc9
Kaushall-raut <KaushalRaut.code@gmail.com>
2024-08-05 22:24:36 +0530

    saving work

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ echo $_REQUEST['pass'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insert Page</title>
+    <title>Delete Page</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
       rel="stylesheet"
